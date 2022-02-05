9a3941503990b97b1024edb135c1ed7ff22f4f4c
nicknovick <nnnovick@gmail.com>
2022-02-05 19:42:06 +0100

    Added temperature to display Added time to console output Commented out GPIO code responsible for LEDs Refresh time changed to 3s On critical CO2 shows CO2 longer instead of delay after ventilator

--- co2.py
+++ co2.py
@@ -9,7 +9,7 @@ import os
 from pathlib import Path
 import subprocess
 import time
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import mh_z19
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
@@ -17,16 +17,16 @@ from luma.oled.device import sh1106
 from PIL import ImageFont, Image, ImageOps
 
 
-GPIO.setmode(GPIO.BCM)
-RED = 18
-YELLOW = 1
-GREEN = 24
+# GPIO.setmode(GPIO.BCM)
+# RED = 18
+# YELLOW = 1
+# GREEN = 24
 CO2_CRITICAL = 1000
 CO2_WARNING = 700
 CO2_LOW = 430
-GPIO.setup(RED, GPIO.OUT, initial=False)
-GPIO.setup(YELLOW, GPIO.OUT, initial=False)
-GPIO.setup(GREEN, GPIO.OUT, initial=False)
+# GPIO.setup(RED, GPIO.OUT, initial=False)
+# GPIO.setup(YELLOW, GPIO.OUT, initial=False)
+# GPIO.setup(GREEN, GPIO.OUT, initial=False)
 SERIAL = i2c(port=1, address=0x3C)
 DEVICE = sh1106(SERIAL)
 FONT = ImageFont.truetype('coolvetica rg.otf', 26)
@@ -58,35 +58,34 @@ def set_green():
     """Sets the indication for good CO2 condition"""
     subprocess.run("sudo sh -c 'echo 1 > /sys/class/leds/led0/brightness'", shell=True) #green on
     subprocess.run("sudo sh -c 'echo 0 > /sys/class/leds/led1/brightness'", shell=True) #red off
-    GPIO.output(GREEN, True)
-    GPIO.output(YELLOW, False)
-    GPIO.output(RED, False)
+    # GPIO.output(GREEN, True)
+    # GPIO.output(YELLOW, False)
+    # GPIO.output(RED, False)
 
 def set_yellow():
     """Sets the indication for warning CO2 condition"""
     subprocess.run("sudo sh -c 'echo 1 > /sys/class/leds/led0/brightness'", shell=True) #green on
     subprocess.run("sudo sh -c 'echo 1 > /sys/class/leds/led1/brightness'", shell=True) #red on
-    GPIO.output(GREEN, False)
-    GPIO.output(YELLOW, True)
-    GPIO.output(RED, False)
+    # GPIO.output(GREEN, False)
+    # GPIO.output(YELLOW, True)
+    # GPIO.output(RED, False)
 
 def set_red():
     """Sets the indication for critical CO2 condition"""
     subprocess.run("sudo sh -c 'echo 0 > /sys/class/leds/led0/brightness'", shell=True) #green off
     subprocess.run("sudo sh -c 'echo 1 > /sys/class/leds/led1/brightness'", shell=True) #red on
-    GPIO.output(GREEN, False)
-    GPIO.output(YELLOW, False)
-    GPIO.output(RED, True)
-    time.sleep(2)
+    # GPIO.output(GREEN, False)
+    # GPIO.output(YELLOW, False)
+    # GPIO.output(RED, True)
     rotate_vent()
 
 def set_none():
     """Turns the indication off"""
     subprocess.run("sudo sh -c 'echo 0 > /sys/class/leds/led0/brightness'", shell=True) #green off
     subprocess.run("sudo sh -c 'echo 0 > /sys/class/leds/led1/brightness'", shell=True) #red off
-    GPIO.output(GREEN, False)
-    GPIO.output(YELLOW, False)
-    GPIO.output(RED, False)
+    # GPIO.output(GREEN, False)
+    # GPIO.output(YELLOW, False)
+    # GPIO.output(RED, False)
 
 try:
     image = Image.open(IMG_PATH)
@@ -99,29 +98,31 @@ try:
     BACKGROUND.paste(IMG, POSN)
 
     while True:
-        X = mh_z19.read()
-        VALUE = X["co2"]
-        print(VALUE)
+        X = mh_z19.read_all()
+        CO2_VALUE = X["co2"]
+        TEMP_VALUE = X["temperature"]
+        t = time.localtime()
+        print(CO2_VALUE, "at", time.strftime("%Y-%m-%d %H:%M:%S", t))
         with canvas(DEVICE) as draw:
             draw.text((0, 0), "CO₂", font=FONT, fill="white")
-            draw.text((70, 0), str(VALUE), font=FONT, fill="white")
-            #draw.text((0, 30), "Tmp", font=FONT, fill="white")
-            #draw.text((70, 30), "99", font=FONT, fill="white")
-            #draw.text((100, 30), "°C", font=FONT, fill="white")
+            draw.text((70, 0), str(CO2_VALUE), font=FONT, fill="white")
+            draw.text((0, 30), "Tmp", font=FONT, fill="white")
+            draw.text((70, 30), str(TEMP_VALUE), font=FONT, fill="white")
+            draw.text((100, 30), "°C", font=FONT, fill="white")
 
-        if VALUE > CO2_CRITICAL:
+        time.sleep(3)
+        if CO2_VALUE > CO2_CRITICAL:
             set_red()
-        elif VALUE > CO2_WARNING:
+        elif CO2_VALUE > CO2_WARNING:
             set_yellow()
-        elif VALUE < CO2_LOW:
+        elif CO2_VALUE < CO2_LOW:
             set_green()
             time.sleep(1)
             set_none()
         else:
             set_green()
-        time.sleep(1)
 except BaseException as error:
     print(str(error))
     subprocess.run("echo mmc0 > /sys/class/leds/led0/trigger", shell=True)
     subprocess.run("echo input > /sys/class/leds/led1/trigger", shell=True)
-    GPIO.cleanup()
+    # GPIO.cleanup()
